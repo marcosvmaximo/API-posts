8fa20bd03c52ba245283a1cd0c78ad8b124c39ea
Marcos Maximo <96027900+marcosvmaximo@users.noreply.github.com>
2022-11-14 00:53:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Alura_dotnet7.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var stringConexaoMySql = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<PostsContext>(options =>
+options.UseMySQL(stringConexaoMySql));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
